8bdb2e10e676f71bd46c6fc621dca6d72fc53c09
LouayFarah <louayfarahioi@gmail.com>
2025-05-06 23:43:21 +0300

    Implement catalog service and endpoints for fetching books

---
+++ backend/services/catalog.py
@@ -0,0 +1,34 @@
+import requests
+
+from config import Config
+
+class CatalogService():
+    config = Config()
+    
+    def fetch_books_paginated(self, page_no: int, page_size: int):
+        """
+        Fetches books from the database with pagination.
+        """
+        url = self.config.get_gutendex_base_url() + "/books"
+        params = {
+            "page": page_no
+        }
+        response = requests.get(url, params=params)
+        if response.status_code == 200:
+            return response.json()
+        else:
+            return None
+        
+    def fetch_book_by_id(self, book_id: int):
+        """
+        Fetches a book by its ID from the database.
+        """
+        url = self.config.get_gutendex_base_url() + f"/books"
+        params = {
+            "ids": book_id
+        }
+        response = requests.get(url, params=params)
+        if response.status_code == 200:
+            return response.json()
+        else:
+            return None
