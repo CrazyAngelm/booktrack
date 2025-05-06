fb529183061596756584deb94dd03416f8cfa17b
LouayFarah <louayfarahioi@gmail.com>
2025-05-06 23:30:49 +0300

    Initialize FastAPI application with CORS middleware and root endpoint

---
+++ backend/main.py
@@ -0,0 +1,33 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from dotenv import load_dotenv
+from routers import users_router, catalog_router
+from exceptions import *
+
+
+load_dotenv()  # Load environmental variables
+
+# Create the backend application
+app = FastAPI()
+
+# Allow all origins
+origins = ["*"]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# Include the routers
+app.include_router(users_router)
+app.include_router(catalog_router)
+
+# Add the exceptions handlers
+
+
+# Define the root source
+@app.get("/", tags=["Root"], status_code=200)
+async def root():
+    return {"message": "The BookTrack services are up!"}
