ca44265caaa4cdadcd228978beedc661239fae74
LouayFarah <louayfarahioi@gmail.com>
2025-05-08 23:23:13 +0300

    Fix pagination default value, update book ID endpoint path, and implement search functionality in CatalogService

--- backend/routers/catalog.py
+++ backend/routers/catalog.py
@@ -16,7 +16,7 @@ config = Config()
 
 @catalog_router.get("/api/books", response_model=list[BookRead], status_code=200)
 def fetch_books_paginated_endpoint(
-    page_no: int = 0,
+    page_no: int = 1,
     page_size: int = 36,
     user: AuthenticatedUser = Depends(get_user)
 ):
@@ -27,7 +27,7 @@ def fetch_books_paginated_endpoint(
     return result.get("results", [])
 
 
-@catalog_router.get("/api/books/{book_id}", response_model=BookRead, status_code=200)
+@catalog_router.get("/api/books/book-id/{book_id}", response_model=BookRead, status_code=200)
 def fetch_book_by_id_endpoint(
     book_id: int,
     user: AuthenticatedUser = Depends(get_user)
@@ -36,3 +36,16 @@ def fetch_book_by_id_endpoint(
     if (not result) or (not result.get("results")) or (len(result.get("results")) == 0):
         raise HTTPException(status_code=404, detail=f"Book with id {book_id} not found")
     return result.get("results")[0]
+
+
+@catalog_router.get("/api/books/search", response_model=list[BookRead], status_code=200)
+def search_books_endpoint(
+    query: str,
+    page_no: int = 1,
+    page_size: int = 36,
+    user: AuthenticatedUser = Depends(get_user)
+):
+    result = catalog_service.search_books(query=query, page_no=page_no, page_size=page_size)
+    if not result:
+        raise HTTPException(status_code=404, detail="No books found")
+    return result.get("results", [])
